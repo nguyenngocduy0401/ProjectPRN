57f0f3890d511b54f4aca9c948590a07d84f3951
NguyenDucAnh1908 <nguyenducanhit00@gmail.com>
2023-07-12 16:17:07 +0700

    DA

---
+++ FrmCURDCategory.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace ProjectPRN
+{
+    partial class FrmCURDCategory
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dgvListCatetory = new System.Windows.Forms.DataGridView();
+            blMaHang = new System.Windows.Forms.Label();
+            txtMaHang = new System.Windows.Forms.TextBox();
+            lbTenHang = new System.Windows.Forms.Label();
+            txtTenHang = new System.Windows.Forms.TextBox();
+            btnAccept = new System.Windows.Forms.Button();
+            btCapNhat = new System.Windows.Forms.Button();
+            btXoa = new System.Windows.Forms.Button();
+            btnCancel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)dgvListCatetory).BeginInit();
+            SuspendLayout();
+            // 
+            // dgvListCatetory
+            // 
+            dgvListCatetory.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvListCatetory.Location = new System.Drawing.Point(12, 98);
+            dgvListCatetory.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            dgvListCatetory.Name = "dgvListCatetory";
+            dgvListCatetory.RowHeadersWidth = 51;
+            dgvListCatetory.RowTemplate.Height = 25;
+            dgvListCatetory.Size = new System.Drawing.Size(347, 260);
+            dgvListCatetory.TabIndex = 34;
+            dgvListCatetory.CellDoubleClick += dgvListCatetory_CellDoubleClick;
+            // 
+            // blMaHang
+            // 
+            blMaHang.AutoSize = true;
+            blMaHang.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            blMaHang.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            blMaHang.Location = new System.Drawing.Point(391, 98);
+            blMaHang.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            blMaHang.Name = "blMaHang";
+            blMaHang.Size = new System.Drawing.Size(111, 25);
+            blMaHang.TabIndex = 36;
+            blMaHang.Text = "Type code:";
+            // 
+            // txtMaHang
+            // 
+            txtMaHang.Location = new System.Drawing.Point(391, 154);
+            txtMaHang.Name = "txtMaHang";
+            txtMaHang.Size = new System.Drawing.Size(122, 27);
+            txtMaHang.TabIndex = 37;
+            // 
+            // lbTenHang
+            // 
+            lbTenHang.AutoSize = true;
+            lbTenHang.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            lbTenHang.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            lbTenHang.Location = new System.Drawing.Point(391, 211);
+            lbTenHang.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            lbTenHang.Name = "lbTenHang";
+            lbTenHang.Size = new System.Drawing.Size(117, 25);
+            lbTenHang.TabIndex = 38;
+            lbTenHang.Text = "Type name:";
+            // 
+            // txtTenHang
+            // 
+            txtTenHang.Location = new System.Drawing.Point(391, 266);
+            txtTenHang.Name = "txtTenHang";
+            txtTenHang.Size = new System.Drawing.Size(184, 27);
+            txtTenHang.TabIndex = 39;
+            // 
+            // btnAccept
+            // 
+            btnAccept.Anchor = System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right;
+            btnAccept.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            btnAccept.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            btnAccept.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            btnAccept.Location = new System.Drawing.Point(647, 98);
+            btnAccept.Margin = new System.Windows.Forms.Padding(5);
+            btnAccept.Name = "btnAccept";
+            btnAccept.Size = new System.Drawing.Size(139, 36);
+            btnAccept.TabIndex = 40;
+            btnAccept.Text = "Create";
+            btnAccept.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            btnAccept.UseVisualStyleBackColor = true;
+            btnAccept.Click += btnAccept_Click;
+            // 
+            // btCapNhat
+            // 
+            btCapNhat.Anchor = System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right;
+            btCapNhat.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            btCapNhat.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            btCapNhat.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            btCapNhat.Location = new System.Drawing.Point(647, 190);
+            btCapNhat.Margin = new System.Windows.Forms.Padding(5);
+            btCapNhat.Name = "btCapNhat";
+            btCapNhat.Size = new System.Drawing.Size(139, 36);
+            btCapNhat.TabIndex = 41;
+            btCapNhat.Text = "Update";
+            btCapNhat.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            btCapNhat.UseVisualStyleBackColor = true;
+            btCapNhat.Click += btCapNhat_Click;
+            // 
+            // btXoa
+            // 
+            btXoa.Anchor = System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right;
+            btXoa.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            btXoa.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            btXoa.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            btXoa.Location = new System.Drawing.Point(647, 266);
+            btXoa.Margin = new System.Windows.Forms.Padding(5);
+            btXoa.Name = "btXoa";
+            btXoa.Size = new System.Drawing.Size(139, 36);
+            btXoa.TabIndex = 42;
+            btXoa.Text = "Delete";
+            btXoa.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            btXoa.UseVisualStyleBackColor = true;
+            btXoa.Click += btXoa_Click;
+            // 
+            // btnCancel
+            // 
+            btnCancel.Anchor = System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right;
+            btnCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            btnCancel.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            btnCancel.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            btnCancel.Location = new System.Drawing.Point(391, 322);
+            btnCancel.Margin = new System.Windows.Forms.Padding(5);
+            btnCancel.Name = "btnCancel";
+            btnCancel.Size = new System.Drawing.Size(139, 36);
+            btnCancel.TabIndex = 43;
+            btnCancel.Text = "Cancel";
+            btnCancel.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            btnCancel.UseVisualStyleBackColor = true;
+            btnCancel.Click += btnCancel_Click;
+            // 
+            // FrmCURDCategory
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(800, 450);
+            Controls.Add(btnCancel);
+            Controls.Add(btXoa);
+            Controls.Add(btCapNhat);
+            Controls.Add(btnAccept);
+            Controls.Add(txtTenHang);
+            Controls.Add(lbTenHang);
+            Controls.Add(txtMaHang);
+            Controls.Add(blMaHang);
+            Controls.Add(dgvListCatetory);
+            Name = "FrmCURDCategory";
+            Text = "FrmCURDCategory";
+            ((System.ComponentModel.ISupportInitialize)dgvListCatetory).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvListCatetory;
+        private System.Windows.Forms.Label blMaHang;
+        private System.Windows.Forms.TextBox txtMaHang;
+        private System.Windows.Forms.Label lbTenHang;
+        private System.Windows.Forms.TextBox txtTenHang;
+        private System.Windows.Forms.Button btnAccept;
+        private System.Windows.Forms.Button btCapNhat;
+        private System.Windows.Forms.Button btXoa;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
---
+++ FrmCURDCategory.cs
@@ -0,0 +1,202 @@
+﻿using Repository.Models;
+using Repository.Service;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace ProjectPRN
+{
+    public partial class FrmCURDCategory : Form
+    {
+        private CoffeeManagerContext _context = new CoffeeManagerContext();
+        private CategoryServices _categoryservices = new CategoryServices();
+        List<Category> listProductsOnOrder = new List<Category>();
+        public FrmCURDCategory()
+        {
+            InitializeComponent();
+            var listCatetory = _context.Categories.ToList();
+            dgvListCatetory.DataSource = new BindingSource { DataSource = listCatetory };
+            txtMaHang.Visible = false;
+            blMaHang.Visible = false;
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            txtTenHang.Text = "";
+            txtMaHang.Text = "";
+            btnAccept.Visible = true;
+            txtMaHang.Visible = false;
+            blMaHang.Visible = false;
+        }
+
+        private void btCapNhat_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int CategoryID = int.Parse(txtMaHang.Text);
+                String catetoryName = txtTenHang.Text;
+                var Category = _categoryservices.GetAll().Where(p => p.CategoryId.Equals(CategoryID)).FirstOrDefault();
+
+                bool flag = false;
+                if (string.IsNullOrEmpty(catetoryName))
+                {
+                    MessageBox.Show("Please enter catetory name", "Notification", MessageBoxButtons.OK);
+                    flag = true;
+                }
+
+                TextInfo textInfo = CultureInfo.CurrentCulture.TextInfo;
+                String pattern = @"\s+";
+                catetoryName = Regex.Replace(catetoryName, pattern, " ");
+                catetoryName = Regex.Replace(catetoryName, pattern, " ");
+                catetoryName = textInfo.ToTitleCase(catetoryName);
+                catetoryName = textInfo.ToTitleCase(catetoryName);
+
+
+
+                if (Category != null && !flag)
+                {
+                    Category.CategoryName = catetoryName; Category.CategoryId = CategoryID;
+                    _categoryservices.Update(Category);
+                }
+                else
+                {
+                    MessageBox.Show("No record match!!", "Notification", MessageBoxButtons.OK);
+                }
+                dgvListCatetory.Refresh();
+                txtTenHang.Text = "";
+                dgvListCatetory.DataSource = new BindingSource() { DataSource = _categoryservices.GetAll() };
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Choose the line you want to update", "Notification", MessageBoxButtons.OK);
+            }
+        }
+
+        private void btnAccept_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                String catetoryName = txtTenHang.Text;
+                bool flag = false;
+                if (string.IsNullOrEmpty(catetoryName))
+                {
+                    MessageBox.Show("Please enter catetory name", "Notification", MessageBoxButtons.OK);
+                    flag = true;
+                }
+
+                TextInfo textInfo = CultureInfo.CurrentCulture.TextInfo;
+                String pattern = @"\s+";
+                catetoryName = Regex.Replace(catetoryName, pattern, " ");
+                catetoryName = Regex.Replace(catetoryName, pattern, " ");
+                catetoryName = textInfo.ToTitleCase(catetoryName);
+                catetoryName = textInfo.ToTitleCase(catetoryName);
+
+
+
+                if (!flag)
+                {
+                    var catetory = new Category
+                    {
+                        CategoryName = catetoryName,
+
+
+                    };
+                    _categoryservices.Create(catetory);
+                    dgvListCatetory.DataSource = new BindingSource() { DataSource = _categoryservices.GetAll() };
+
+
+                    txtTenHang.Text = "";
+
+
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Please enter the information", "Notification", MessageBoxButtons.OK);
+            }
+        }
+
+        private void btXoa_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int CategoryID = int.Parse(txtMaHang.Text);
+                var Category = _categoryservices.GetAll().Where(p => p.CategoryId.Equals(CategoryID)).FirstOrDefault();
+
+                if (Category != null)
+                {
+                    DialogResult result = MessageBox.Show("Delete this category?", "Notification", MessageBoxButtons.OKCancel);
+
+                    switch (result)
+                    {
+                        case DialogResult.None:
+                            break;
+                        case DialogResult.OK:
+                            _categoryservices.Delete(Category);
+                            dgvListCatetory.DataSource = new BindingSource() { DataSource = _categoryservices.GetAll() };
+                            dgvListCatetory.ClearSelection(); // Bỏ chọn dòng hiện tại trên dgvListCatetory
+                            break;
+                        case DialogResult.Cancel:
+                            break;
+                        case DialogResult.Abort:
+                            break;
+                        case DialogResult.Retry:
+                            break;
+                        case DialogResult.Ignore:
+                            break;
+                        case DialogResult.Yes:
+                            break;
+                        case DialogResult.No:
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                else
+                {   
+                    MessageBox.Show("No record match!", "Notification", MessageBoxButtons.OK);
+                }
+
+                dgvListCatetory.Refresh();
+
+                txtTenHang.Text = "";
+                txtMaHang.Text = "";
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Choose the line you want to delete", "Notification", MessageBoxButtons.OK);
+            }
+        }
+
+
+
+        private void dgvListCatetory_CellMouseDoubleClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+
+        }
+
+        private void dgvListCatetory_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            btnAccept.Visible = false;
+            var CatetoryID = dgvListCatetory.Rows[e.RowIndex].Cells[0].Value;
+            var Category = _categoryservices.GetAll().Where(p => p.CategoryId.Equals(CatetoryID)).FirstOrDefault();
+            if (Category != null)
+            {
+                txtTenHang.Text = Category.CategoryName.ToString();
+                txtMaHang.Text = Category.CategoryId.ToString();
+                blMaHang.Visible = true;
+                txtMaHang.Visible = true;
+            }
+        }
+    }
+}
